1ed6f524e62e8d8995f500da808d2e0691551821
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-01-27 22:26:58 -0500

    Initial project setup Initial commit for project with all supporting documentation, license, authors, and workflow/documentation. This is simply the initial version of the project with a default game for build pipeline. License will need to be setup remotely for unity project but this can be done easily.

---
+++ Assets/Scripts/UI/NetworkActions.cs
@@ -0,0 +1,93 @@
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.UI
+{
+    /// <summary>
+    /// Actions to control the network manager via UI buttons
+    /// </summary>
+    public class NetworkActions : MonoBehaviour
+    {
+        /// <summary>
+        /// Network manager to control network state
+        /// </summary>
+        public NetworkManager manager { get; private set; }
+
+        /// <summary>
+        /// Address to connect to
+        /// </summary>
+        public InputField connectAddress;
+
+        public bool Initialized()
+        {
+            return manager != null;
+        }
+
+        public void StartHost()
+        {
+            // Server + Client
+            // Make sure to not host on web player, web player can't host I think...
+            if (Application.platform != RuntimePlatform.WebGLPlayer && Initialized())
+            {
+                manager.StartHost();
+            }
+        }
+
+        public void StartClient()
+        {
+            if (Initialized())
+            {
+                manager.StartClient();
+                manager.networkAddress = connectAddress.text;
+            }
+        }
+
+        public void StartServer()
+        {
+            if (Initialized())
+            {
+                manager.StartServer();
+            }
+        }
+
+        public void StopClientConnecting()
+        {
+            if (Initialized() && NetworkClient.active)
+            {
+                manager.StopClient();
+            }
+        }
+
+        public void StopClient()
+        {
+            if (Initialized())
+            {
+                if (NetworkServer.active && NetworkClient.isConnected)
+                {
+                    manager.StopHost();
+                }
+                // stop client if client-only
+                else if (NetworkClient.isConnected)
+                {
+                    manager.StopClient();
+                }
+                // stop server if server-only
+                else if (NetworkServer.active)
+                {
+                    manager.StopServer();
+                }
+            }
+        }
+
+        public void Update()
+        {
+            // Check if manager is attached, if not find a network manager and attach it
+            if (!Initialized())
+            {
+                manager = GameObject.FindObjectOfType<NetworkManager>();
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# Working Title: Project Cage
+
+[![CC BY-NC-SA 4.0][cc-by-nc-sa-shield]][cc-by-nc-sa]
+
+Project for Game Jam 2021 with IGDA with other students at UC. Working Title: Project Cage
+
+## Authors
+All of the members of our group working on the project
+* Alex Heyl
+* Allan Ong
+* Cara McCarthy
+* Daniel Sennett
+* Jake Glass
+* Joseph Mak
+* Nicholas Maltbie
+* Samuel Scherer
+* Zachary Scherer
+
+
+## Development Environment
+
+We are going to use Unity3D. For full information on the development environment and how to setup for contributing, see [Development.md](Development.md)
+
+## Contribution Guidelines
+
+Please see [Contributing.md](Contributing.md) for details about adding to the project.
+
+## License
+
+[![CC BY-NC-SA 4.0][cc-by-nc-sa-image]][cc-by-nc-sa]
+
+This work is licensed under a
+[Creative Commons Attribution- NonCommercial-ShareAlike 4.0 International License][cc-by-nc-sa].
+
+[cc-by-nc-sa]: http://creativecommons.org/licenses/by-nc-sa/4.0/
+[cc-by-nc-sa-image]: https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-nc-sa.svg
+[cc-by-nc-sa-shield]: https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-lightgrey.svg
