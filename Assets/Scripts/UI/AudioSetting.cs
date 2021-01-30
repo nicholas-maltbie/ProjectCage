b7994e084b2801e6b4542041f205170f61b49045
Samuel Scherer <swiimii962@gmail.com>
2021-01-29 22:35:21 -0500

    Add volume sliders

---
+++ Assets/Scripts/UI/AudioSetting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSetting : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public string volumeGroup = "Master";
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat(volumeGroup, Mathf.Log10(sliderValue) * 20);
+    }
+
+}
