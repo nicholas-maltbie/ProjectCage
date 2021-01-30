6dfefc7fca32d789b578c360eb0714c5d02eb730
Samuel Scherer <swiimii962@gmail.com>
2021-01-30 16:21:30 -0500

    Reformatted code to pass pipeline

--- Assets/Scripts/Panda.cs
+++ Assets/Scripts/Panda.cs
@@ -19,11 +19,11 @@ public class Panda : Animal
 
         var isPlayerHoldingFood = other.CompareTag("Player") && other.GetComponent<HoldObject>().heldItem == favoriteFood;
         var isFoodItem = other.CompareTag("Food Item") && other.GetComponent<ItemState>().item == favoriteFood;
-        if ( isPlayerHoldingFood || isFoodItem )
+        if (isPlayerHoldingFood || isFoodItem)
         {
             target = other.gameObject;
         }
-        
+
     }
     private void OnTriggerExit2D(Collider2D other)
     {
