3187bd2dcf2c9da2484de2264bda8135e287e49a
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-01-29 21:46:48 -0500

    Top Down Character Movement (#17)
    * Began CharacterMovement Branch
    
    :)
    
    * Added a default player movement script
    Added the default player movement to the character
    
    * Added a camera attach script
    
    Updated character to work with networking and added script to attach camera to your player
    
    * Added pushable cube for the game
    
    * Fixed spacing because I'm bad at spacing
    
    * Fixed character movement normalization
    
    Co-authored-by: Balmages <jmak2222@gmail.com>

---
+++ Assets/Scripts/Character/CameraAttach.cs
@@ -0,0 +1,29 @@
+using Mirror;
+using UnityEngine;
+
+namespace Scripts.Character
+{
+    /// <summary>
+    /// Attach the camera to this game object on connect
+    /// Detach on disconnect
+    /// </summary>
+    public class CameraAttach : NetworkBehaviour
+    {
+        public override void OnStartClient()
+        {
+            if (isLocalPlayer)
+            {
+                Camera.main.transform.parent = transform;
+                Camera.main.transform.position = new Vector3(0, 0, Camera.main.transform.position.z);
+            }
+        }
+        public override void OnStopClient()
+        {
+            if (isLocalPlayer)
+            {
+                Camera.main.transform.parent = null;
+                Camera.main.transform.position = new Vector3(0, 0, Camera.main.transform.position.z);
+            }
+        }
+    }
+}
