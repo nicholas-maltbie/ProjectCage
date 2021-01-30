9d56ae6168f4e7cf6c05153f50a9af69b68d6c5b
Samuel Scherer <swiimii962@gmail.com>
2021-01-30 12:55:54 -0500

    Panda animates and acts correctly now.

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -4,14 +4,23 @@ using UnityEngine.AI;
 using UnityEngine;
 using Mirror;
 
+public enum Direction
+{
+    Right = 1,
+
+    Left = -1
+}
 public abstract class Animal : NetworkBehaviour
 {
     // Start is called before the first frame update
 
     public GameObject target;
-    private NavMeshAgent agent;
 
+    // This variable denotes whether a sprite is drawn facing Right or Left by default.
+    public Direction initialFacingDirection = Direction.Left;
+    private NavMeshAgent agent;
     private Animator animator;
+    private bool isFacingRight;
 
     protected virtual void Start()
     {
@@ -24,16 +33,21 @@ public abstract class Animal : NetworkBehaviour
     // Update is called once per frame
     protected virtual void Update()
     {
-        if(target)
+        if (isServer)
         {
-            agent.SetDestination(target.transform.position);
-            Debug.Log("No navigation target.");
+            if (target)
+            {
+                agent.SetDestination(target.transform.position);
+            }
+
+            DebugDrawPath(agent.path.corners);
+
+            animator.SetFloat("MoveX", agent.velocity.x);
+            SetSpriteFlip(agent.velocity.x);
+            animator.SetFloat("MoveY", agent.velocity.y);
+            var isWalking = agent.remainingDistance > 0.1 ? true : false;
+            animator.SetBool("Walking", isWalking);
         }
-        DebugDrawPath(agent.path.corners);
-        animator.SetFloat("MoveX", agent.velocity.x);
-        animator.SetFloat("MoveY", agent.velocity.y);
-        var isWalking = target ? true : false;
-        animator.SetBool("Walking", isWalking);
     }
     protected static void DebugDrawPath(Vector3[] corners)
     {
@@ -44,5 +58,25 @@ public abstract class Animal : NetworkBehaviour
             Debug.DrawLine(corners[i], corners[i + 1], Color.blue);
         }
         Debug.DrawLine(corners[0], corners[1], Color.red);
+    }
+
+    protected void SetSpriteFlip(float xdirection)
+    {
+        var shouldFlip = false;
+        var sr = GetComponent<SpriteRenderer>();
+        if (initialFacingDirection == Direction.Right)
+        {
+            // started facing right, is moving left
+            if (xdirection < 0)
+            {
+                shouldFlip = true;
+            }
+        }
+        // started facing left, is moving right
+        else if (xdirection > 0)
+        {
+            shouldFlip = true;
+        }
+        sr.flipX = shouldFlip;
     }
 }
