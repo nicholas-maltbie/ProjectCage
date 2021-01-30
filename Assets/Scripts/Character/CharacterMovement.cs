cd8d65189ae403d922352b4e9c49a515d68bba05
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-01-29 21:22:44 -0500

    Fixed spacing because I'm bad at spacing

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -3,38 +3,38 @@ using UnityEngine;
 
 namespace Scripts.Character
 {
-   public class CharacterMovement : NetworkBehaviour
-   {
-      private Rigidbody2D body;
-      private float horizontal;
-      private float vertical;
+    public class CharacterMovement : NetworkBehaviour
+    {
+        private Rigidbody2D body;
+        private float horizontal;
+        private float vertical;
 
-      public float runSpeed = 20.0f;
+        public float runSpeed = 20.0f;
 
-      void Start ()
-      {
-         body = GetComponent<Rigidbody2D>();
-      }
+        void Start()
+        {
+            body = GetComponent<Rigidbody2D>();
+        }
 
-      void Update()
-      {
-         if (isLocalPlayer)
-         {
-            horizontal = Input.GetAxisRaw("Horizontal");
-            vertical = Input.GetAxisRaw("Vertical");
-         }
-      }
+        void Update()
+        {
+            if (isLocalPlayer)
+            {
+                horizontal = Input.GetAxisRaw("Horizontal");
+                vertical = Input.GetAxisRaw("Vertical");
+            }
+        }
 
-      void FixedUpdate()
-      {
-         if (isLocalPlayer)
-         {
-            Vector2 movement = new Vector2(horizontal, vertical);
-            // Make sure the vector is not larger than 1
-            movement = movement.magnitude > 1 ? movement / movement.magnitude : movement;
+        void FixedUpdate()
+        {
+            if (isLocalPlayer)
+            {
+                Vector2 movement = new Vector2(horizontal, vertical);
+                // Make sure the vector is not larger than 1
+                movement = movement.magnitude > 1 ? movement / movement.magnitude : movement;
 
-            body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
-         }
-      }
-   }
+                body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+            }
+        }
+    }
 }
