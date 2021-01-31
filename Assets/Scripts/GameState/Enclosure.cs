f7afc39dab08ddd34477368786eaa5b8e5d9f298
Samuel Scherer <swiimii962@gmail.com>
2021-01-30 21:21:32 -0500

    Victory State working

--- Assets/Scripts/GameState/Enclosure.cs
+++ Assets/Scripts/GameState/Enclosure.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 using Mirror;
 using Scripts.Animals;
 
-public class Enclosure : NetworkBehaviour 
+public class Enclosure : NetworkBehaviour
 {
     // Requires Rigidbody2D, and a collider with isTrigger enabled.
     public AnimalSpecies enclosureSpecies;
+    [SyncVar]
     public int numberOfSpecies;
     [SyncVar]
     public int numberCaptured = 0;
 
     private EnclosureManager enclosureManager;
-    private void Start() 
+    private void Start()
     {
         enclosureManager = FindObjectOfType<EnclosureManager>();
     }
@@ -23,25 +24,25 @@ public class Enclosure : NetworkBehaviour
         bool isCorrectAnimal = other.GetComponent<Animal>() && other.GetComponent<Animal>().species == enclosureSpecies;
         return isCorrectAnimal && !other.isTrigger;
     }
-    private void OnTriggerEnter2D(Collider2D other) 
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if(isServer && IsCorrectTarget(other))
+        if (isServer && IsCorrectTarget(other))
         {
             numberCaptured += 1;
-            if(numberOfSpecies == numberCaptured)
+            if (numberOfSpecies >= numberCaptured)
             {
                 enclosureManager.UpdateCaptureStatus(enclosureSpecies, true);
             }
-        
+
         }
     }
-    private void OnTriggerExit2D(Collider2D other) 
+    private void OnTriggerExit2D(Collider2D other)
     {
-        if(isServer && IsCorrectTarget(other))
+        if (isServer && IsCorrectTarget(other))
         {
             numberCaptured -= 1;
             enclosureManager.UpdateCaptureStatus(enclosureSpecies, false);
-        
+
         }
     }
 
