ab345d0d9247cec2533a7b3ab636a2ff13e5d48d
Samuel Scherer <swiimii962@gmail.com>
2021-01-30 21:06:48 -0500

    Add enclosure and manager classes for winstate

---
+++ Assets/Scripts/GameState/EnclosureManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Scripts.Animals;
+using Mirror;
+public class EnclosureManager : NetworkBehaviour
+{
+    public Enclosure penguinEnclosure, pandaEnclosure, lionEnclosure;
+    public bool arePenguinsFound = false, arePandasFound = false, areLionsFound = false;
+
+    private void Start() 
+    {
+        penguinEnclosure.numberOfSpecies = FindObjectsOfType<Penguin>().Length;
+        penguinEnclosure.enclosureSpecies = AnimalSpecies.Penguin;
+        pandaEnclosure.numberOfSpecies = FindObjectsOfType<Panda>().Length;
+        pandaEnclosure.enclosureSpecies = AnimalSpecies.Panda;
+        // lionEnclosure.numberOfSpecies = FindObjectsOfType<Lion>().Length;
+    }
+
+    public void UpdateCaptureStatus(AnimalSpecies species, bool status)
+    {
+        switch (species)
+        {
+            case AnimalSpecies.Penguin:
+                arePenguinsFound = status;
+                break;
+            case AnimalSpecies.Panda:
+                arePandasFound = status;
+                break;
+            case AnimalSpecies.Lion:
+                areLionsFound = status;
+                break;
+        }
+        if(isServer)
+        {
+            if(CheckVictoryState())
+            {
+                print("Win!");
+            }
+        }
+    }
+
+    public bool CheckVictoryState()
+    {
+        return arePenguinsFound && arePandasFound;
+    }
+}
