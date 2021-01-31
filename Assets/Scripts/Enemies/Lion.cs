40a24ef243690ccfce78e0ca0504aeddc957d026
Samuel Scherer <37094203+swiimii@users.noreply.github.com>
2021-01-31 13:24:44 -0500

    Feature/add lion (#72)
    * Add lion prefab WIP
    
    * Modify Lion Prefab point value
    
    * Modify lion one more time
    
    * Allow lion animations to loop

---
+++ Assets/Scripts/Enemies/Lion.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Scripts.Animals;
+using Scripts.Items;
+using Scripts.Character;
+
+public class Lion : Animal
+{
+    public Scripts.Items.Item favoriteFood2;
+    public override void Start()
+    {
+        species = AnimalSpecies.Lion;
+        base.Start();
+    }
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        other.gameObject.SendMessage("KillCharacter", SendMessageOptions.DontRequireReceiver);
+    }
+
+    protected override void OnTriggerStay2D(Collider2D other)
+    {
+        var isPlayer = other.CompareTag("Player");
+        var isMeat = other.GetComponent<ItemState>() && (other.GetComponent<ItemState>().item == favoriteFood || other.GetComponent<ItemState>().item == favoriteFood2);
+        var isFoodItem = other.CompareTag("Food Item") && isMeat;
+        if (isPlayer || isFoodItem)
+        {
+            target = other.gameObject;
+            if (isPlayer)
+            {
+                scoreCredit = target;
+            }
+            else // is food item
+            {
+                scoreCredit = target.GetComponent<ItemState>().scoreCredit;
+            }
+        }
+    }
+}
